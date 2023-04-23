d9e38c9b13319a4af34018f63dade4b081d9601d
EduardoGuevara124 <michael.guevara.mtz.12@outlook.com>
2023-04-23 09:47:46 +0700

    openai added

--- chatController.js
+++ chatController.js
@@ -1,9 +1,24 @@
 const methods = {};
+const {Configuration, OpenAIApi} = require("openai");
 
-methods.getAnswer = (req) => {
-    console.log(req.body);
+const configuration = new Configuration({
+    apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+methods.getAnswer = (req) => {    
     return new Promise(async(resolve, reject) => {
-        resolve("success");
+        try {
+            openai.createCompletion({
+                model: "text-davinci-001",
+                prompt: req.body.question,
+            }).then((data) => {
+                resolve({status: 200, message: data.data.choices[0].text});
+            });
+        } catch(e) {
+            reject(e);
+        }
     });
 }
 
